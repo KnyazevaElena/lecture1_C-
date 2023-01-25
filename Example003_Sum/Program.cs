bfc6b8ff2561fb45075494488744d63ec45eeb54
Elena Knyazeva <lenen241989sc@inbox.ru>
2023-01-25 21:20:55 +0700

    sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 20;
-int NumberB = 30;
+﻿int NumberA = new Random().Next (1,10); // 1,2,3,4,5,6,7,8,9
+Console.WriteLine (NumberA);
+int NumberB = new Random().Next (1,10); 
+Console.WriteLine (NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
